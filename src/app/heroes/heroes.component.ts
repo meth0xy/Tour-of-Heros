12cc5a5f9ca61269aa8350eb998923ea7f0f61c9
meth0xy <den-erginos@hotmail.com>
2023-12-11 17:25:36 +0100

    Create a Heroes List Component which renders a list of heros when selecting a hero it should display the details with an input field to edit the name it has also a button which clsoes the details which is passed as @Output Event Emitter from its child details component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroDetailsComponent } from '../hero-details/hero-details.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HeroDetailsComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
 
 export class HeroesComponent {
-  hero : Hero = {
-    name: 'Wisdom',
-    id: 1,
+  heroes = HEROES;
+  currentSelectedHero : Hero | undefined;
+  selectHero (hero: Hero | undefined) {
+    this.currentSelectedHero = hero;
   }
 }
