6518be289f614629c121ce9902195fbb74f69a69
meth0xy <den-erginos@hotmail.com>
2023-12-12 14:24:26 +0100

    Add a Message Service into a Service & Other Components * Add Message Service in HeroService to log messages into an array

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(private messageService : MessageService) { }
 
   getHeros(): Observable<Hero[]> {
+    this.messageService.add('heroService: fetched heroes');
     return of(HEROES);
-  }
+  };
 
-  constructor() { }
 }
