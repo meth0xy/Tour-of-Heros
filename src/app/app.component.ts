b27a6d1c22ede02a6f03c8380533f85f2954e03c
meth0xy <den-erginos@hotmail.com>
2023-12-12 16:57:15 +0100

    Add Routerlinks for Dashboard Heros / HeroList in HeroComponent * Add Stylings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent, MessagesComponent],
+  imports: [CommonModule, RouterOutlet, MessagesComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
